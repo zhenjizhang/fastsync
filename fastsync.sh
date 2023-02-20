b2cce459437d58df11add280fc0e3a43819021ae
zhenjizhang <zhenjizhang0126@gmail.com>
2023-02-20 15:25:30 +0800

    Update fastsync.sh

--- fastsync.sh
+++ fastsync.sh
@@ -2,15 +2,18 @@
 #author: zhangzhenji 2023-02-19 
 
 YOUR_TMP_BRANCH=$1
-REMOTE_HOST=$2
-REMOTE_GIT_PATH=$3
+JUMPER_HOST=$2
+REMOTE_HOST=$3
+REMOTE_GIT_PATH=$4
 echo "YOUR_TMP_BRANCH: ${YOUR_TMP_BRANCH}"
+echo "JUMPER_HOST: ${JUMPER_HOST}"
 echo "REMOTE_HOST: ${REMOTE_HOST}"
 echo "REMOTE_GIT_PATH: ${REMOTE_GIT_PATH}"
 git clone https://github.com/zhenjizhang/fastsync.git
 
 sed -i '' "s/your-tmp-branch/${YOUR_TMP_BRANCH}/g" fastsync/resource/localSync.sh
 sed -i '' "s/your-tmp-branch/${YOUR_TMP_BRANCH}/g" fastsync/resource/remoteSync.sh
+sed -i '' "s/jumper-host/${JUMPER_HOST}/g" fastsync/resource/remoteSync.sh
 sed -i '' "s/remote-host-addr/${REMOTE_HOST}/g" fastsync/resource/remoteSync.sh
 sed -i '' "s/remote-git-path/${REMOTE_GIT_PATH}/g" fastsync/resource/remoteSync.sh
 mv fastsync/resource/* .git/
